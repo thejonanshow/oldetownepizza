946a8df65f501613c79d8aeb9507dc91da3ea01c
Honzie <hans.sprecher@gmail.com>
2011-04-06 20:53:14 -0600

    backgrounds added

--- db/schema.rb
+++ db/schema.rb
@@ -23,86 +23,86 @@ ActiveRecord::Schema.define(:version => 20110407013826) do
   add_index "image_pages", ["page_id"], :name => "index_image_pages_on_page_id"
 
   create_table "images", :force => true do |t|
-    t.string   "image_mime_type"
-    t.string   "image_name"
-    t.integer  "image_size"
-    t.integer  "image_width"
-    t.integer  "image_height"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "image_uid"
-    t.string   "image_ext"
+    t.string    "image_mime_type"
+    t.string    "image_name"
+    t.integer   "image_size"
+    t.integer   "image_width"
+    t.integer   "image_height"
+    t.timestamp "created_at"
+    t.timestamp "updated_at"
+    t.string    "image_uid"
+    t.string    "image_ext"
   end
 
   create_table "inquiries", :force => true do |t|
-    t.string   "name"
-    t.string   "email"
-    t.string   "phone"
-    t.text     "message"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.boolean  "spam",       :default => false
+    t.string    "name"
+    t.string    "email"
+    t.string    "phone"
+    t.text      "message"
+    t.timestamp "created_at"
+    t.timestamp "updated_at"
+    t.boolean   "spam",       :default => false
   end
 
   add_index "inquiries", ["id"], :name => "index_inquiries_on_id"
 
   create_table "inquiry_settings", :force => true do |t|
-    t.string   "name"
-    t.text     "value"
-    t.boolean  "destroyable"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.string    "name"
+    t.text      "value"
+    t.boolean   "destroyable"
+    t.timestamp "created_at"
+    t.timestamp "updated_at"
   end
 
   create_table "page_part_translations", :force => true do |t|
-    t.integer  "page_part_id"
-    t.string   "locale"
-    t.text     "body"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.integer   "page_part_id"
+    t.string    "locale"
+    t.text      "body"
+    t.timestamp "created_at"
+    t.timestamp "updated_at"
   end
 
   add_index "page_part_translations", ["page_part_id"], :name => "index_page_part_translations_on_page_part_id"
 
   create_table "page_parts", :force => true do |t|
-    t.integer  "page_id"
-    t.string   "title"
-    t.text     "body"
-    t.integer  "position"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.integer   "page_id"
+    t.string    "title"
+    t.text      "body"
+    t.integer   "position"
+    t.timestamp "created_at"
+    t.timestamp "updated_at"
   end
 
   add_index "page_parts", ["id"], :name => "index_page_parts_on_id"
   add_index "page_parts", ["page_id"], :name => "index_page_parts_on_page_id"
 
   create_table "page_translations", :force => true do |t|
-    t.integer  "page_id"
-    t.string   "locale"
-    t.string   "title"
-    t.string   "custom_title"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.integer   "page_id"
+    t.string    "locale"
+    t.string    "title"
+    t.string    "custom_title"
+    t.timestamp "created_at"
+    t.timestamp "updated_at"
   end
 
   add_index "page_translations", ["page_id"], :name => "index_page_translations_on_page_id"
 
   create_table "pages", :force => true do |t|
-    t.integer  "parent_id"
-    t.integer  "position"
-    t.string   "path"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.boolean  "show_in_menu",        :default => true
-    t.string   "link_url"
-    t.string   "menu_match"
-    t.boolean  "deletable",           :default => true
-    t.string   "custom_title_type",   :default => "none"
-    t.boolean  "draft",               :default => false
-    t.boolean  "skip_to_first_child", :default => false
-    t.integer  "lft"
-    t.integer  "rgt"
-    t.integer  "depth"
+    t.integer   "parent_id"
+    t.integer   "position"
+    t.string    "path"
+    t.timestamp "created_at"
+    t.timestamp "updated_at"
+    t.boolean   "show_in_menu",        :default => true
+    t.string    "link_url"
+    t.string    "menu_match"
+    t.boolean   "deletable",           :default => true
+    t.string    "custom_title_type",   :default => "none"
+    t.boolean   "draft",               :default => false
+    t.boolean   "skip_to_first_child", :default => false
+    t.integer   "lft"
+    t.integer   "rgt"
+    t.integer   "depth"
   end
 
   add_index "pages", ["depth"], :name => "index_pages_on_depth"
@@ -112,27 +112,27 @@ ActiveRecord::Schema.define(:version => 20110407013826) do
   add_index "pages", ["rgt"], :name => "index_pages_on_rgt"
 
   create_table "refinery_settings", :force => true do |t|
-    t.string   "name"
-    t.text     "value"
-    t.boolean  "destroyable",             :default => true
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "scoping"
-    t.boolean  "restricted",              :default => false
-    t.string   "callback_proc_as_string"
-    t.string   "form_value_type"
+    t.string    "name"
+    t.text      "value"
+    t.boolean   "destroyable",             :default => true
+    t.timestamp "created_at"
+    t.timestamp "updated_at"
+    t.string    "scoping"
+    t.boolean   "restricted",              :default => false
+    t.string    "callback_proc_as_string"
+    t.string    "form_value_type"
   end
 
   add_index "refinery_settings", ["name"], :name => "index_refinery_settings_on_name"
 
   create_table "resources", :force => true do |t|
-    t.string   "file_mime_type"
-    t.string   "file_name"
-    t.integer  "file_size"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "file_uid"
-    t.string   "file_ext"
+    t.string    "file_mime_type"
+    t.string    "file_name"
+    t.integer   "file_size"
+    t.timestamp "created_at"
+    t.timestamp "updated_at"
+    t.string    "file_uid"
+    t.string    "file_ext"
   end
 
   create_table "roles", :force => true do |t|
@@ -148,26 +148,26 @@ ActiveRecord::Schema.define(:version => 20110407013826) do
   add_index "roles_users", ["user_id", "role_id"], :name => "index_roles_users_on_user_id_and_role_id"
 
   create_table "seo_meta", :force => true do |t|
-    t.integer  "seo_meta_id"
-    t.string   "seo_meta_type"
-    t.string   "browser_title"
-    t.string   "meta_keywords"
-    t.text     "meta_description"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.integer   "seo_meta_id"
+    t.string    "seo_meta_type"
+    t.string    "browser_title"
+    t.string    "meta_keywords"
+    t.text      "meta_description"
+    t.timestamp "created_at"
+    t.timestamp "updated_at"
   end
 
   add_index "seo_meta", ["id"], :name => "index_seo_meta_on_id"
   add_index "seo_meta", ["seo_meta_id", "seo_meta_type"], :name => "index_seo_meta_on_seo_meta_id_and_seo_meta_type"
 
   create_table "slugs", :force => true do |t|
-    t.string   "name"
-    t.integer  "sluggable_id"
-    t.integer  "sequence",                     :default => 1, :null => false
-    t.string   "sluggable_type", :limit => 40
-    t.string   "scope",          :limit => 40
-    t.datetime "created_at"
-    t.string   "locale"
+    t.string    "name"
+    t.integer   "sluggable_id"
+    t.integer   "sequence",                     :default => 1, :null => false
+    t.string    "sluggable_type", :limit => 40
+    t.string    "scope",          :limit => 40
+    t.timestamp "created_at"
+    t.string    "locale"
   end
 
   add_index "slugs", ["locale"], :name => "index_slugs_on_locale"
@@ -184,21 +184,21 @@ ActiveRecord::Schema.define(:version => 20110407013826) do
   add_index "user_plugins", ["user_id", "name"], :name => "index_unique_user_plugins", :unique => true
 
   create_table "users", :force => true do |t|
-    t.string   "username",             :null => false
-    t.string   "email",                :null => false
-    t.string   "encrypted_password",   :null => false
-    t.string   "persistence_token"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "perishable_token"
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.integer  "sign_in_count"
-    t.string   "remember_token"
-    t.string   "reset_password_token"
-    t.datetime "remember_created_at"
+    t.string    "username",             :null => false
+    t.string    "email",                :null => false
+    t.string    "encrypted_password",   :null => false
+    t.string    "persistence_token"
+    t.timestamp "created_at"
+    t.timestamp "updated_at"
+    t.string    "perishable_token"
+    t.timestamp "current_sign_in_at"
+    t.timestamp "last_sign_in_at"
+    t.string    "current_sign_in_ip"
+    t.string    "last_sign_in_ip"
+    t.integer   "sign_in_count"
+    t.string    "remember_token"
+    t.string    "reset_password_token"
+    t.timestamp "remember_created_at"
   end
 
   add_index "users", ["id"], :name => "index_users_on_id"
