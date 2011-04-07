94664e66ae62944ca4a560d3a64ef2021e3d49a1
1337807 <jonan.scheffler@g5platform.com>
2011-04-06 20:37:00 -0700

    Redirect all requests to actual URL

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+    before_filter :ensure_domain
+
+    APP_DOMAIN = 'oldetownepizzabend.com'
+
+    def ensure_domain
+      if request.env['HTTP_HOST'] != APP_DOMAIN
+        redirect_to "http://#{APP_DOMAIN}", :status => 301
+      end
+    end
 end
