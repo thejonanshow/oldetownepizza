bb74713801be3fbfbc8ef513063f44b8adf83a9e
Honzie <hans.sprecher@gmail.com>
2011-04-13 19:38:33 -0600

    moving typography from home.css to application.css

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,6 @@ class ApplicationController < ActionController::Base
     APP_DOMAIN = 'oldetownepizzabend.com'
 
     def ensure_domain
-      if request.env['HTTP_HOST'] != APP_DOMAIN
-        redirect_to "http://#{APP_DOMAIN}", :status => 301
-      end
+      true
     end
 end
