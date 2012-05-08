8f85ea9dac46d1480a3f824f4abf4325779d01a5
Jonan Scheffler <jonanscheffler@gmail.com>
2012-05-07 21:26:49 -0400

    Gemset update.

--- config/application.rb
+++ config/application.rb
@@ -38,5 +38,6 @@ module Oldetownepizza
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
+    config.action_mailer.delivery_method = :smtp
   end
 end
