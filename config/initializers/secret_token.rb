d30e2129babe6e47859d6e79d2f92183cddd59ef
Jonan Scheffler <jonanscheffler@gmail.com>
2013-11-19 17:12:52 -0800

    Clean up index.

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Oldetownepizza::Application.config.secret_token = '1cfcf8074622b33914cedcb9aa04d818217154f031ea63a6a524d2977f3510acc6bcff2acdac507191b2c47bd44b6bad085e28e64ff783a772a31e6a822ee7ba'
